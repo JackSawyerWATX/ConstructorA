2c721cd8904761927b51c3334f0ef3df025633d7
Jack Sawyer <84543199+JackSawyerWATX@users.noreply.github.com>
2022-09-16 07:25:17 -0700

    changed content

--- Main.java
+++ Main.java
@@ -2,15 +2,22 @@ public class Main {
     String x;
     String y;
     String z;
+    String a;
+    String b;
+    String c;
   
     public Main() {
-      x = "farts";
-      y = "My";
-      z = "stink";
+      x = "so";
+      y = "I'm";
+      z = "exahusted";
+      a = "must";
+      b = "I";
+      c = "go on";
     }
   
     public static void main(String[] args) {
       Main what = new Main();
       System.out.println(what.y + " " + what.x + " " + what.z + ".");
+      System.out.println(what.b + " " + what.a + " " + what.c + ".");
     }
   }
